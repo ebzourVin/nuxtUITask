f008a52c18aed3b2880871335f655399b5feca8c
ebzourVin <169066915+ebzourVin@users.noreply.github.com>
2024-05-15 18:28:30 +0300

    Add files via upload

---
+++ PageFooter.vue
@@ -0,0 +1,5 @@
+<template>
+    <UContainer class="mx-auto content-center">
+      <p>Need help? <NuxtLink to="/ContactUS" class="text-[#EE7203]">Contact Us</NuxtLink> </p>
+    </UContainer>
+</template>
