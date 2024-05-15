162ec8414b0d4815b730e749ca197d33613ebf6a
ebzourVin <169066915+ebzourVin@users.noreply.github.com>
2024-05-15 18:32:47 +0300

    Delete PageFooter.vue

--- PageFooter.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <UContainer class="mx-auto content-center">
-      <p>Need help? <NuxtLink to="/ContactUS" class="text-[#EE7203]">Contact Us</NuxtLink> </p>
-    </UContainer>
-</template>
